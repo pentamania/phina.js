ff17e53fdff5ec475f41a7a91d061f498bf7c497
pentamania <tanigami7220@gmail.com>
2019-02-26 23:47:12 +0900

    modify uglify task to catch error

--- gulp/tasks/uglify.js
+++ gulp/tasks/uglify.js
@@ -3,21 +3,34 @@ var uglify  = require('gulp-uglify');
 var header  = require('gulp-header');
 var rename  = require('gulp-rename');
 var util    = require('gulp-util');
+var pump    = require('pump');
 
 var config = require('../config');
 
-gulp.task('uglify', function() {
-  return gulp.src(config.uglify.target)
-    .pipe(uglify())
-    .pipe(header(config.banner, {
+gulp.task('uglify', function(cb) {
+  pump([
+    gulp.src(config.uglify.target),
+    uglify(),
+    header(config.banner, {
       pkg: config.package,
-    }))
-    .pipe(rename({
+    }),
+    rename({
       extname: '.min.js'
-    }))
-    .pipe(gulp.dest(config.uglify.output))
-    .on('end', function() {
-      util.log(util.colors.blue('finish'));
-    });
+    }),
+    gulp.dest(config.uglify.output)
+  ], cb);
+
+  // return gulp.src(config.uglify.target)
+  //   .pipe(uglify())
+  //   .pipe(header(config.banner, {
+  //     pkg: config.package,
+  //   }))
+  //   .pipe(rename({
+  //     extname: '.min.js'
+  //   }))
+  //   .pipe(gulp.dest(config.uglify.output))
+  //   .on('end', function() {
+  //     util.log(util.colors.blue('finish'));
+  //   });
 });
 
