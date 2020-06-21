cd3d5b7c98561b8ec59072cf3ed6d7d1baa308a7
pentamania <tanigami7220@gmail.com>
2020-06-21 19:35:27 +0900

    convert ManagerScene to ESM
    - support passing es-module scene class

--- src/game/managerscene.js
+++ src/game/managerscene.js
@@ -1,172 +1,178 @@
+import phina from "../phina";
+import { $extend } from "../core/object";
+import { format } from "../core/string";
+import { Scene } from "../app/scene";
 
-phina.namespace(function() {
+/**
+ * @class phina.game.ManagerScene
+ * @extends phina.app.Scene
+ */
+export class ManagerScene extends Scene {
 
   /**
-   * @class phina.game.ManagerScene
-   * @extends phina.app.Scene
+   * @constructor
    */
-  phina.define('phina.game.ManagerScene', {
-    superClass: 'phina.app.Scene',
-    /**
-     * @constructor
-     */
-    init: function(params) {
-      this.superInit();
+  constructor(params) {
+    super();
 
-      this.setScenes(params.scenes);
+    this.setScenes(params.scenes);
 
-      this.on("enter", function() {
-        this.gotoScene(params.startLabel || 0);
-      }.bind(this));
+    this.on("enter", function() {
+      this.gotoScene(params.startLabel || 0);
+    }.bind(this));
 
-      this.on("resume", this.onnext.bind(this));
+    this.on("resume", this.onnext.bind(this));
 
-      this.commonArguments = {};
-    },
+    this.commonArguments = {};
+  }
 
+  /**
+   * scenes をセット
+   */
+  setScenes(scenes) {
+    this.scenes = scenes;
+    this.sceneIndex = 0;
 
-    /**
-     * scenes をセット
-     */
-    setScenes: function(scenes) {
-      this.scenes = scenes;
-      this.sceneIndex = 0;
+    return this;
+  }
 
-      return this;
-    },
+  /**
+   * Sceneクラスをインスタンス化して返す
+   * 
+   * @param  {[type]} data  [description]
+   * @return {[type]}       [description]
+   */
+  _instantiateScene(data, args) {
+    // Scene初期化引数
+    var initArguments = $extend.call({}, data.arguments, args);
+    // var initArguments = {}.$extend(data.arguments, args);
 
-
-    replaceScene: function(label, args) {
-      var index = (typeof label == 'string') ? this.labelToIndex(label) : label||0;
-
-      var data = this.scenes[index];
-
-      if (!data) {
-        console.error('phina.js error: `{0}` に対応するシーンがありません.'.format(label));
-      }
-
-      var klass = phina.using(data.className);
+    var scene, klass;
+    if (typeof data.className === 'string') {
+      // phina.define、あるいはglobal(window)に直接定義されたクラスの文字列
+      klass = phina.using(data.className);
       if (typeof klass !== 'function') {
         klass = phina.using('phina.game.' + data.className);
       }
+      // scene = klass.call(null, initArguments);
+    } else if (typeof data.className === 'function') {
+      // 関数の場合、純粋なclassと見なす
+      klass = data.className;
+    }
+    scene = new klass(initArguments);
 
-      var initArguments = {}.$extend(data.arguments, args);
-      var scene = klass.call(null, initArguments);
-      if (!scene.nextLabel) {
-          scene.nextLabel = data.nextLabel;
-      }
-      if (!scene.nextArguments) {
-          scene.nextArguments = data.nextArguments;
-      }
-      this.app.replaceScene(scene);
+    if (!scene.nextLabel) {
+      scene.nextLabel = data.nextLabel;
+    }
+    if (!scene.nextArguments) {
+      scene.nextArguments = data.nextArguments;
+    }
 
-      this.sceneIndex = index;
+    return scene
+  }
 
-      return this;
-    },
+  /**
+   * Sceneクラスをインスタンス化してappにreplaceSceneさせる
+   * 
+   * @param  {string|number} label シーンの対応ラベル
+   * @param  {any} [args] Sceneにわたす引数がある場合に指定
+   * @return {this}
+   */
+  replaceScene(label, args) {
+    var index = (typeof label == 'string') ? this.labelToIndex(label) : label||0;
+    if (!this.scenes[index]) {
+      console.error(format.call('phina.js error: `{0}` に対応するシーンがありません.', label));
+    }
+    var scene = this._instantiateScene(this.scenes[index], args)
+    this.app.replaceScene(scene);
+    this.sceneIndex = index;
 
+    return this;
+  }
 
-    /**
-     * index(or label) のシーンへ飛ぶ
-     */
-    gotoScene: function(label, args) {
-      var index = (typeof label == 'string') ? this.labelToIndex(label) : label||0;
+  /**
+   * index(or label) のシーンへ飛ぶ
+   * replaceSceneとの違いはapp.replaceSceneではなく、app.pushSceneを実行する点
+   */
+  gotoScene(label, args) {
+    var index = (typeof label == 'string') ? this.labelToIndex(label) : label||0;
+    if (!this.scenes[index]) {
+      console.error(format.call('phina.js error: `{0}` に対応するシーンがありません.', label));
+    }
+    var scene = this._instantiateScene(this.scenes[index], args)
+    this.app.pushScene(scene);
+    this.sceneIndex = index;
 
-      var data = this.scenes[index];
+    return this;
+  }
 
-      if (!data) {
-        console.error('phina.js error: `{0}` に対応するシーンがありません.'.format(label));
-      }
+  /**
+   * 次のシーンへ飛ぶ
+   */
+  gotoNext(args) {
+    var data = this.scenes[this.sceneIndex];
+    var nextIndex = null;
 
-      var klass = phina.using(data.className);
-      if (typeof klass !== 'function') {
-        klass = phina.using('phina.game.' + data.className);
-      }
+    // 次のラベルが設定されていた場合
+    if (data.nextLabel) {
+        nextIndex = this.labelToIndex(data.nextLabel);
+    }
+    // 次のシーンに遷移
+    else if (this.sceneIndex+1 < this.scenes.length) {
+        nextIndex = this.sceneIndex+1;
+    }
 
-      var initArguments = {}.$extend(data.arguments, args);
-      var scene = klass.call(null, initArguments);
-      if (!scene.nextLabel) {
-          scene.nextLabel = data.nextLabel;
-      }
-      if (!scene.nextArguments) {
-          scene.nextArguments = data.nextArguments;
-      }
-      this.app.pushScene(scene);
+    if (nextIndex !== null) {
+        this.gotoScene(nextIndex, args);
+    }
+    else {
+        this.flare("finish");
+    }
 
-      this.sceneIndex = index;
+    return this;
+  }
 
-      return this;
-    },
+  /**
+   * シーンインデックスを取得
+   */
+  getCurrentIndex() {
+    return this.sceneIndex;
+  }
 
-    /**
-     * 次のシーンへ飛ぶ
-     */
-    gotoNext: function(args) {
-      var data = this.scenes[this.sceneIndex];
-      var nextIndex = null;
+  /**
+   * シーンラベルを取得
+   */
+  getCurrentLabel() {
+    return this.scenes[this.sceneIndex].label;
+  }
 
-      // 次のラベルが設定されていた場合
-      if (data.nextLabel) {
-          nextIndex = this.labelToIndex(data.nextLabel);
-      }
-      // 次のシーンに遷移
-      else if (this.sceneIndex+1 < this.scenes.length) {
-          nextIndex = this.sceneIndex+1;
-      }
+  /**
+   * ラベルからインデックスに変換
+   */
+  labelToIndex(label) {
+    var data = this.scenes.filter(function(data) {
+      return data.label == label;
+    })[0];
 
-      if (nextIndex !== null) {
-          this.gotoScene(nextIndex, args);
-      }
-      else {
-          this.flare("finish");
-      }
+    return this.scenes.indexOf(data);
+  }
 
-      return this;
-    },
+  /**
+   * インデックスからラベルに変換
+   */
+  indexToLabel(index) {
+    return this.scenes[index].label;
+  }
 
-    /**
-     * シーンインデックスを取得
-     */
-    getCurrentIndex: function() {
-      return this.sceneIndex;
-    },
+  onnext(e) {
+    var nextLabel = e.prevScene.nextLabel;
+    var nextArguments = e.prevScene.nextArguments;
+    if (nextLabel) {
+      this.gotoScene(nextLabel, nextArguments);
+    }
+    else {
+      this.gotoNext(nextArguments);
+    }
+  }
 
-    /**
-     * シーンラベルを取得
-     */
-    getCurrentLabel: function() {
-      return this.scenes[this.sceneIndex].label;
-    },
-
-    /**
-     * ラベルからインデックスに変換
-     */
-    labelToIndex: function(label) {
-      var data = this.scenes.filter(function(data) {
-        return data.label == label;
-      })[0];
-
-      return this.scenes.indexOf(data);
-    },
-
-    /**
-     * インデックスからラベルに変換
-     */
-    indexToLabel: function(index) {
-      return this.scenes[index].label;
-    },
-
-    onnext: function(e) {
-      var nextLabel = e.prevScene.nextLabel;
-      var nextArguments = e.prevScene.nextArguments;
-      if (nextLabel) {
-        this.gotoScene(nextLabel, nextArguments);
-      }
-      else {
-        this.gotoNext(nextArguments);
-      }
-    },
-
-  });
-
-});
+}
