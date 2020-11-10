850d7f91fb54de030756092d57536b7c1d3be4ec
pentamania <tanigami7220@gmail.com>
2020-11-10 15:32:45 +0900

    convert GameApp to ESM

--- src/game/gameapp.js
+++ src/game/gameapp.js
@@ -1,108 +1,160 @@
-phina.namespace(function() {
+import phina from "../phina";
+import { $safe, $extend, forIn } from "../core/object";
+import { each } from "../core/array";
+import { CanvasApp } from "../display/canvasapp";
+import { DisplayScene } from "../display/displayscene";
+import { ManagerScene } from "./managerscene";
+import { LoadingScene } from "./loadingscene";
+import { SplashScene } from "./splashscene";
+import { TitleScene } from "./titlescene";
+import { PauseScene } from "./pausescene";
+import { ResultScene } from "./resultscene";
 
-  /**
-   * @class phina.game.GameApp
-   * @extends phina.display.CanvasApp
-   */
-  phina.define('phina.game.GameApp', {
-    superClass: 'phina.display.CanvasApp',
+/**
+ * デフォルトのain class
+ */
+class DefaultMainScene extends DisplayScene {
+  constructor(options) {
+    super(options);
+    console.log('This is MainScene');
+  }
+};
 
-    init: function(options) {
+/**
+ * クラスがphina.defineによって定義されているかどうかを返します
+ * 
+ * @param {string} className クラス名。phina.game[className]で定義されているかも調べる
+ * @return {string|boolean} 定義されてればそのままクラス名文字列を返す
+ */
+function isGameClassDefined(className) {
+  if (
+    typeof phina.using(className) === 'function'
+    || typeof phina.using('phina.game.' + className) === 'function'
+  ) {
+    return className
+  }
+  return false;
+}
 
-      options = (options || {}).$safe({
-        startLabel: 'title',
+/**
+ * @class phina.game.GameApp
+ * @extends phina.display.CanvasApp
+ */
+export class GameApp extends CanvasApp {
+
+  constructor(options) {
+    options = $safe.call(options || {}, {
+    // options = (options || {}).$safe({
+      startLabel: 'title',
+    });
+    super(options);
+
+    var startLabel = options.startLabel || 'title';
+
+    var scenes = options.scenes || [
+      {
+        className: isGameClassDefined("SplashScene") || SplashScene,
+        label: 'splash',
+        nextLabel: 'title',
+      },
+      {
+        className: isGameClassDefined("TitleScene") || TitleScene,
+        label: 'title',
+        nextLabel: 'main',
+      },
+      {
+        className: isGameClassDefined("MainScene") || DefaultMainScene,
+        label: 'main',
+        nextLabel: 'result',
+      },
+      {
+        className: isGameClassDefined("ResultScene") || ResultScene,
+        label: 'result',
+        nextLabel: 'title',
+      },
+    ];
+
+    scenes = each.call(scenes, function(s) {
+      s.arguments = s.arguments || options;
+    });
+
+    var scene = new ManagerScene({
+      startLabel: startLabel,
+      scenes: scenes,
+    });
+
+    if (options.assets) {
+      // ローディング：esm版では独自のLoadingSceneはオプションで渡せるようにする
+
+      var loadingOptions = $extend.call({}, options, {
+      // var loadingOptions = ({}).$extend(options, {
+        exitType: '',
       });
-      this.superInit(options);
+      // グローバル定義のLoadingSceneを探す（従来）
+      // -> なければオプションをチェック 
+      // -> これもなければデフォルトのLoadingSceneを使う
+      var definedLoadingClass = phina.using("LoadingScene") || phina.using("phina.game.LoadingScene");
+      var loading = (typeof definedLoadingClass === 'function') 
+        ? definedLoadingClass(loadingOptions)
+        : (options.loadingScene != null)
+          ? new options.loadingScene(loadingOptions) 
+          : new LoadingScene(loadingOptions)
+      ;
+      this.replaceScene(loading);
 
-      var startLabel = options.startLabel || 'title';
-
-      var scenes = options.scenes || [
-        {
-          className: 'SplashScene',
-          label: 'splash',
-          nextLabel: 'title',
-        },
-
-        {
-          className: 'TitleScene',
-          label: 'title',
-          nextLabel: 'main',
-        },
-        {
-          className: 'MainScene',
-          label: 'main',
-          nextLabel: 'result',
-        },
-        {
-          className: 'ResultScene',
-          label: 'result',
-          nextLabel: 'title',
-        },
-      ];
-
-      scenes = scenes.each(function(s) {
-        s.arguments = s.arguments || options;
-      });
-
-      var scene = phina.game.ManagerScene({
-        startLabel: startLabel,
-        scenes: scenes,
-      });
-
-      if (options.assets) {
-        var loadingOptions = ({}).$extend(options, {
-          exitType: '',
-        });
-        var loadingClass = phina.global.LoadingScene || phina.game.LoadingScene;
-        var loading = loadingClass(loadingOptions);
-        this.replaceScene(loading);
-
-        loading.onloaded = function() {
-          this.replaceScene(scene);
-          if (options.debug) {
-            this._enableDebugger();
-          }
-        }.bind(this);
-
-      }
-      else {
+      loading.onloaded = function() {
         this.replaceScene(scene);
         if (options.debug) {
           this._enableDebugger();
         }
+      }.bind(this);
+    }
+    else {
+      this.replaceScene(scene);
+      if (options.debug) {
+        this._enableDebugger();
       }
+    }
 
-      // 自動でポーズする
-      if (options.autoPause) {
-        this.on('blur', function() {
-          var pauseScene = phina.game.PauseScene();
-          this.pushScene(pauseScene);
-        });
-      }
-    },
+    // 自動でポーズする
+    // esm版では独自のポーズシーンはオプションで渡す
+    // 引数が渡せないのは元から
+    if (options.autoPause) {
+      this.on('blur', function() {
+        var definedPauseScene = phina.using("phina.game.PauseScene");
+        var pauseScene = (typeof definedPauseScene === 'function') 
+          ? definedPauseScene() 
+          : (options.pauseScene) 
+            ? new options.pauseScene() 
+            : new PauseScene()
+        this.pushScene(pauseScene);
+      });
+    }
+  }
 
-    _enableDebugger: function() {
-      if (this.gui) return ;
+  _enableDebugger() {
+    if (this.gui) return ;
 
-      this.enableDatGUI(function(gui) {
-        var f = gui.addFolder('scenes');
-        var funcs = {};
-        this.rootScene.scenes.each(function(scene) {
-          funcs[scene.label] = function() {
-            this.rootScene.replaceScene(scene.label);
-            console.log(this._scenes.length);
-          }.bind(this);
-          return scene;
-        }, this);
+    this.enableDatGUI(function(gui) {
+      var f = gui.addFolder('scenes');
+      var funcs = {};
+      this.rootScene.scenes.forEach(function(scene) {
+      // this.rootScene.scenes.each(function(scene) {
+        funcs[scene.label] = function() {
+          this.rootScene.replaceScene(scene.label);
+          console.log(this._scenes.length);
+        }.bind(this);
+        return scene;
+      }, this);
 
-        funcs.forIn(function(key, value) {
-          f.add(funcs, key);
-        });
-        f.open();
+      forIn.call(funcs, function(key, value) {
+      // funcs.forIn(function(key, value) {
+        f.add(funcs, key);
+      });
+      f.open();
 
-        this.gui = gui;
-      }.bind(this));
-    },
-  });
+      this.gui = gui;
+    }.bind(this));
+  }
 
-});
+}
