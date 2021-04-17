10ab188cea7494e7a46ce14b9994a856d9051b0b
pentamania <tanigami7220@gmail.com>
2021-04-17 14:30:09 +0900

    update accessory/frameanimation JSDoc comment
    Add @protected tag, examples, descriptions.
    Change '_updateFrame' to @protected

--- src/accessory/frameanimation.js
+++ src/accessory/frameanimation.js
@@ -2,6 +2,7 @@ import { Accessory } from "./accessory";
 import { AssetManager } from "../asset/assetmanager";
 
 /**
+ * Spriteクラスなど、FrameAnimationのtargetとして適正な型
  * @typedef {{
  *   srcRect: import("../geom/rect").Rect
  *   width: number
@@ -12,6 +13,19 @@ import { AssetManager } from "../asset/assetmanager";
 /**
  * @class phina.accessory.FrameAnimation
  * _extends phina.accessory.Accessory
+ * 
+ * フレームアニメーション制御を行うAccessory派生クラス
+ * 
+ * 予めロード（パース）したスプライトシートJSONデータを使い、
+ * Spriteクラスのフレーム範囲を制御することでアニメーションを実現する
+ * @see https://qiita.com/alkn203/items/a287c7524193f5f4ca90
+ * 
+ * @example
+ * // 予め"player_ss"という名前でスプライトシート画像、Jsonデータをアセット登録しておく
+ * // player_ssには"walk"という名前のアニメーションを定義
+ * const target = new phina.display.Sprite("player_ss");
+ * const frameAnim = new phina.accessory.FrameAnimation("player_ss").attachTo(target);
+ * frameAnim.gotoAndPlay("walk");
  */
 export class FrameAnimation extends Accessory {
 
@@ -25,33 +39,67 @@ export class FrameAnimation extends Accessory {
     /** @type {FrameAnimationTarget} */
     this.target
 
-    /** @type {string | number} */
-    this.currentAnimationName
-
-    /** @type {number} */
-    this.currentFrameIndex
-
-    /** @type {number} */
-    this.frame
-
     /**
      * スプライトシートオブジェクト
+     * 
      * @type {import('../asset/spritesheet').SpriteSheet}
      */
     this.ss = AssetManager.get('spritesheet', ss);
 
-    /** @type {boolean} */
+    /**
+     * 再生中のアニメーションのデータオブジェクト
+     * 
+     * @type {import("../asset/spritesheet").SpriteSheetAnimationData | null}
+     */
+    this.currentAnimation
+
+    /**
+     * 再生中のアニメーション名
+     * 
+     * @type {(string | number) | null}
+     */
+    this.currentAnimationName
+
+    /**
+     * 停止状態
+     * 
+     * @type {boolean}
+     */
     this.paused = true;
 
-    /** @type {boolean} */
-    this.finished = false;
-
-    /** @type {boolean} */
+    /**
+     * フレームサイズに合わせて対象の幅・高さを変えるかどうか
+     * 
+     * @type {boolean}
+     */
     this.fit = true;
+
+    /**
+     * 現在のアニメーションフレームを表すインデックス値
+     * 
+     * @type {number}
+     * @protected
+     */
+    this.currentFrameIndex
+
+    /**
+     * アニメーション更新用のアプリフレームのカウント値
+     * 
+     * @type {number}
+     * @protected
+     */
+    this.frame
+
+    /**
+     * 終了フラグ：trueの時はupdate時にcurrentFrameIndexがリセットされる
+     * 
+     * @type {boolean}
+     * @protected
+     */
+    this.finished = false;
   }
 
   /**
-   * 更新関数
    * @param {*} _app Appクラスインスタンス
    */
   update(_app) {
@@ -72,8 +120,10 @@ export class FrameAnimation extends Accessory {
   }
 
   /**
+   * 指定アニメーションを再生
+   * 
    * @param {string | number} name アニメーション名
-   * @param {boolean} [keep=true] アニメーションがすでに再生中の場合、何もしないかどうか
+   * @param {boolean} [keep=true] 同名アニメーションがすでに再生中の場合、そのままにするかどうか
    * @returns {this}
    */
   gotoAndPlay(name, keep) {
@@ -95,6 +145,8 @@ export class FrameAnimation extends Accessory {
   }
 
   /**
+   * 指定アニメーション及びその冒頭フレームをセット後、停止状態にする
+   * 
    * @param {string} name アニメーション名
    * @returns {this}
    */
@@ -111,9 +163,9 @@ export class FrameAnimation extends Accessory {
   }
 
   /**
-   * @private
-   * フレーム更新
+   * フレーム更新処理
    * 
+   * @protected
    * @returns {void}
    */
   _updateFrame() {
