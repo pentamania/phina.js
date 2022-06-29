03883908226d79f358dfce2fe9c987eb6782b9d2
pentamania <tanigami7220@gmail.com>
2022-06-29 10:01:13 +0900

    asset/assetloader: convert code to ES2015+ style
    conver var -> let/const

--- src/asset/assetloader.js
+++ src/asset/assetloader.js
@@ -74,13 +74,13 @@ export class AssetLoader extends EventDispatcher {
    * @returns {Flow}
    */
   load(params) {
-    var self = this;
+    const self = this;
 
     /** @type {Flow|Promise<any>[]} */
-    var flows = [];
+    const flows = [];
 
-    var counter = 0;
-    var length = 0;
+    let counter = 0;
+    let length = 0;
     forIn.call(params, function(_type, assets) {
     // params.forIn(function(type, assets) {
       length += Object.keys(assets).length;
@@ -90,8 +90,8 @@ export class AssetLoader extends EventDispatcher {
     // params.forIn(function(type, assets) {
       forIn.call(assets, function(key, value) {
       // assets.forIn(function(key, value) {
-        var func = AssetLoader.assetLoadFunctions[type];
-        var flow = func(key, value);
+        const func = AssetLoader.assetLoadFunctions[type];
+        const flow = func(key, value);
         flow.then(function(asset) {
           if (self.cache) {
             AssetManager.set(type, key, asset);
@@ -117,9 +117,9 @@ export class AssetLoader extends EventDispatcher {
           // params.forIn(function(type, assets) {
             forIn.call(assets, function(key, value) {
             // assets.forIn(function(key, value) {
-              var asset = AssetManager.get(type, key);
+              const asset = AssetManager.get(type, key);
               if (asset.loadError) {
-                var dummy = AssetManager.get(type, 'dummy');
+                const dummy = AssetManager.get(type, 'dummy');
                 if (dummy) {
                   if (dummy.loadError) {
                     dummy.loadDummy();
@@ -158,45 +158,45 @@ export class AssetLoader extends EventDispatcher {
  */
 AssetLoader.assetLoadFunctions = {
   image: function(key, path) {
-    var texture = new Texture();
-    var flow = texture.load(path);
+    const texture = new Texture();
+    const flow = texture.load(path);
     return flow;
   },
   sound: function(key, path) {
-    var sound = new Sound();
-    var flow = sound.load(path);
+    const sound = new Sound();
+    const flow = sound.load(path);
     return flow;
   },
   spritesheet: function(key, path) {
-    var ss = new SpriteSheet();
-    var flow = ss.load(path);
+    const ss = new SpriteSheet();
+    const flow = ss.load(path);
     return flow;
   },
   script: function(key, path) {
-    var script = new Script();
+    const script = new Script();
     return script.load(path);
   },
   font: function(key, path) {
-    var font = new Font();
+    const font = new Font();
     font.setFontName(key);
     return font.load(path);
   },
   json: function(key, path) {
-    var text = new File();
+    const text = new File();
     return text.load({
       path: path,
       dataType: "json",
     });
   },
   xml: function(key, path) {
-    var text = new File();
+    const text = new File();
     return text.load({
       path: path,
       dataType: "xml",
     });
   },
   text: function(key, path) {
-    var text = new File();
+    const text = new File();
     return text.load(path);
   }
 }
