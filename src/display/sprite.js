32d7670d6399cbf84d78b954da8d0092d37144a0
pentamania <tanigami7220@gmail.com>
2022-07-01 14:07:55 +0900

    display/sprite: add setImage empty-image setting feature

--- src/display/sprite.js
+++ src/display/sprite.js
@@ -88,12 +88,18 @@ export class Sprite extends DisplayElement {
    * スプライト元画像を設定
    * 
    * @public
-   * @param {SpriteImageSrc} image
+   * @param {SpriteImageSrc | null} [image]
+   * 無指定・nullの場合は空テクスチャを設定（描画はスキップ）
    * @param {number} [width]
    * @param {number} [height]
    * @returns {this}
    */
   setImage(image, width, height) {
+    if (image == null) {
+      this._image = emptyTexture;
+      return this;
+    }
+
     if (typeof image === 'string') {
       image = AssetManager.get('image', image);
     }
