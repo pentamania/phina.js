40736527c8dccda261c5d14287f03dc3958aadc1
pentamania <tanigami7220@gmail.com>
2023-04-24 19:31:38 +0900

    breaking: Remove ThreeLayer

--- src/display/canvaslayer.js
+++ src/display/canvaslayer.js
@@ -3,7 +3,6 @@ import { Canvas } from "../graphics/canvas"
 import { DisplayElement } from "./displayelement";
 import { CanvasRenderer } from "./canvasrenderer";
 import { Grid } from "../util/grid";
-import phina from "../phina";
 
 /**
  * @class phina.display.Layer
@@ -87,44 +86,5 @@ export class CanvasLayer extends Layer {
       0, 0, image.width, image.height,
       -this.width*this.originX, -this.height*this.originY, this.width, this.height
       );
-  }
-}
-
-var THREE = phina.global['THREE']
-
-/**
- * @class phina.display.ThreeLayer
- * _extends phina.display.Layer
- */
-export class ThreeLayer extends Layer {
-
-  // scene: null,
-  // camera: null,
-  // light: null,
-  // renderer: null,
-
-  constructor(options) {
-    super(options);
-
-    this.scene = new THREE.Scene();
-
-    this.camera = new THREE.PerspectiveCamera( 75, options.width / options.height, 1, 10000 );
-    this.camera.position.z = 1000;
-
-    this.light = new THREE.DirectionalLight( 0xffffff, 1 );
-    this.light.position.set( 1, 1, 1 ).normalize();
-    this.scene.add( this.light );
-
-    this.renderer = new THREE.WebGLRenderer();
-    this.renderer.setClearColor( 0xf0f0f0 );
-    this.renderer.setSize( options.width, options.height );
-
-    this.on('enterframe',
-    /** @this ThreeLayer */
-    function() {
-      this.renderer.render( this.scene, this.camera );
-    });
-
-    this.domElement = this.renderer.domElement;
   }
 }
