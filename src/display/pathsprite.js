8c27327d85cfcb793fa0afab3c8277f66fea2a1e
pentamania <10734131+pentamania@users.noreply.github.com>
2024-12-27 21:14:42 +0900

    add: Add FadingPathSprite class

--- src/display/pathsprite.js
+++ src/display/pathsprite.js
@@ -49,3 +49,57 @@ export class PathSprite extends DisplayElement {
     canvas.restore();
   }
 }
+
+/**
+ * 尾っぽになるほど薄くなるPathSprite
+ */
+export class FadingPathSprite extends PathSprite {
+  /**
+   * 最終定期なα値
+   * @type {number}
+   */
+  minAlpha = 0.1;
+
+  /**
+   * @param {ConstructorParameters<typeof DisplayElement>} params
+   */
+  constructor(params) {
+    super(...params);
+    super.blendMode = "lighter";
+  }
+
+  /**
+   * @override
+   * 描画処理
+   *
+   * @param {import("../graphics/canvas").Canvas} canvas
+   * 本体appのCanvasRenderer.canvas参照
+   */
+  draw(canvas) {
+    if (this.paths.length <= 1) return;
+
+    const { minAlpha, paths, stroke, strokeWidth } = this;
+
+    canvas.save();
+
+    // Reset alpha
+    const alphaStep = (1.0 - minAlpha) / (paths.length - 2);
+    canvas.globalAlpha = minAlpha;
+
+    canvas.strokeStyle = stroke;
+    canvas.lineWidth = strokeWidth;
+    const ctx = canvas.context;
+    ctx.beginPath();
+    for (let i = 1, len = paths.length; i < len; ++i) {
+      const lp = paths[i - 1];
+      let p = paths[i];
+      ctx.moveTo(lp.x, lp.y);
+      ctx.lineTo(p.x, p.y);
+      ctx.stroke();
+      ctx.beginPath();
+      canvas.globalAlpha += alphaStep;
+    }
+
+    canvas.restore();
+  }
+}
